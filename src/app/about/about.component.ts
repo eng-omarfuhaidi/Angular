a414d5069d8de3bad1caf358b05af2842c6e5972
omar alfuhaidi <omaralmunssoe@gmail.com>
2022-08-26 18:24:48 +0300

    Assignment 2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Leader } from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  leader:Leader[];
+  featuredLeader:Leader;
+  constructor(private leaderService:LeaderService) { }
 
   ngOnInit() {
+    this.leader=this.leaderService.getLeaders();
+    this.featuredLeader=this.leaderService.getFeaturedLeader();
   }
 
 }
--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -8,7 +8,8 @@ export class PromotionService {
 
   constructor() { }
 
-  getPromotions(): Promotion[] {
+  getPromotions(): Promotion[] 
+  {
     return PROMOTIONS;
   }
 
