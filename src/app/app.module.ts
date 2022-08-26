1913572ae8734feb2dc9fefd1a17656f67b707ae
omar alfuhaidi <omaralmunssoe@gmail.com>
2022-08-26 23:08:53 +0300

    Tempelate Driven Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,12 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 import { AppComponent } from './app.component';
 
@@ -44,13 +49,17 @@ import { LoginComponent } from './login/login.component';
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    FormsModule,
     AppRoutingModule,
     MatToolbarModule,
     MatListModule,
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule
   ],
   providers: [
     DishService,
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -16,7 +16,7 @@ export class HeaderComponent implements OnInit {
   }
   openLoginForm()
   {
-    this.dialog.open(LoginComponent,{width:'500px',height:'450px'});
+    this.dialog.open(LoginComponent,{width:'500px',height:'380px'});
   }
 
 }
